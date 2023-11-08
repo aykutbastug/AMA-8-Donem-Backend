7ad32c65a72a40f5dcbb86e88b873b2370992442
AYKUT BAŞTUĞ <aykutbastug@gmail.com>
2023-11-08 22:18:07 +0300

    ilk projemi ekledim

---
+++ C#-Programlama-Dili/01-VeriTipleriveDegiskenler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_VeriTipleriveDegiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
