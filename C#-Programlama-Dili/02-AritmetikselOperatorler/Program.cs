8a0999240e4d3fb51cb9f26ca347317f297ac695
ceydabaskural <149702623+ceydabaskural@users.noreply.github.com>
2023-11-15 16:44:09 +0300

    2.ders notları

--- C#-Programlama-Dili/02-AritmetikselOperatorler/Program.cs
+++ C#-Programlama-Dili/02-AritmetikselOperatorler/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _02_AritmetikselOperatorler
+﻿using System;
+
+namespace _02_AritmetikselOperatorler
 {
     internal class Program
     {
@@ -57,8 +59,9 @@
             Console.WriteLine("Sonuç = " + sonuc);
             Console.WriteLine("Sonuç = " + (sayi2 * sayi1));
             Console.WriteLine(((sayi2 * sayi1) + (50 + 20)) + " Sonuç");
+
             //+ operatörü kullanılmadan metinsel değerlerin birleştirilmesi
-            Console.WriteLine($"Toplam Ücret = { sayi1 * sayi2 } TL Sayıların Farkı = {sayi1 - sayi2} Sayıların Toplamı = {sayi1 + sayi2}");
+            Console.WriteLine($"Toplam Ücret = {sayi1 * sayi2} TL Sayıların Farkı = {sayi1 - sayi2} Sayıların Toplamı = {sayi1 + sayi2}");
             #endregion
 
             Console.WriteLine("==========================");
@@ -122,10 +125,72 @@
 
             bool evlimi = true;
             Console.WriteLine("Evli Mi = " + evlimi);
-            Console.WriteLine("Evli Mi = " + !evlimi);
+            Console.WriteLine("Evli Mi = " + !evlimi); /*!--> sadece boolen da tersine çevirme işlemi yapıyor yanı true ise false, false ise true.*/
 
             //Herhangi bir tuşa basılana dek console ekranı açık kalması için aşağıdaki kod satırı kullanılır
             Console.ReadKey();
+
+
+            // Ödev1: Bir bakkalın veresiye defterine tutan bir uygulama yaptığımızı hayal edelim. Bu bağlamda lazım olacak olan değişkenleri tipleriyle birlikte yazınız.
+            //  Müşteri Bilgileri = Adı Soyadı, Telefon, Adres, Toplam Borç, Toplam Alacak
+            //  Ürün Bilgileri = Ürün Adı, Ürün Kodu, Birim Fiyatı, Stok Miktarı, Ölçü Birim(Adet, Kg, Gr vs)
+            //  Müşteri Hareketleri = İşlem Tarihi, Ürün Adı, Birim Fiyatı, Miktarı, Tutarı, Kalan Alacak Tutarı
+
+
+            string customernamesurname = "ceydabaskural";
+            int phonenumber = 0551121654;
+            string address = "İzmir";
+            decimal totaldebt = 1000m;
+            decimal totalreceive = 1500m;
+
+            string productname;
+            productname = "dress";
+            long productcode = 256487613268;
+            int unitprice;
+            unitprice = 1000;
+            long stockamount = 2000;
+            string unitofmeasurement = "pcs";
+
+
+            DateTime date= new DateTime (2023,11,15);
+            productname = "dress";
+            unitprice = 1000;
+            long? unitamount = 5;
+            unitprice = 1000;
+            int remainamount = 0;
+
+
+            Console.WriteLine(customernamesurname);
+            Console.WriteLine(phonenumber);
+            Console.WriteLine(address);
+            Console.WriteLine(totaldebt);
+            Console.WriteLine(totalreceive);
+            Console.WriteLine(productname);
+            Console.WriteLine(productcode);
+            Console.WriteLine(unitprice);
+            Console.WriteLine(stockamount);
+            Console.WriteLine(unitofmeasurement);
+            Console.WriteLine(date);
+            Console.WriteLine(productname);
+            Console.WriteLine(unitprice);
+            Console.WriteLine(unitamount);
+            Console.WriteLine(remainamount);
+
+          
+
+
+
+
+            //Ödev 2 : Bir okul uygulaması yaptığımızı hayal edelim. Bu bağlamda lazım olacak olan değişkenleri tipleriyle birlikte yazınız.
+
+            //  Öğrenci Bilgileri = Adı Soyadı, Kayıt Tarihi, Sınıfı, Not Ortalaması
+            //  Veli Bilgileri = Adi Soyadı, Telefon, Ev Adresi, İş Adresi, Annesi Mi ?
+            //  Ders Bilgileri = Ders Adı, Sayısal Mı ?
+            //  Ders Notları = Öğrenci Adı, Ders Adı, 1.Sınav Notu, 2.Sınav Notu, Not Ortalaması
+
+
+
+
         }
     }
 }
