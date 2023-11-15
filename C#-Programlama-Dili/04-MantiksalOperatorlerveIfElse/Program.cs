a2d68d03dbb5f1707ff8a8fe8e7002be6e479143
aykutbastug <aykutbastug@gmail.com>
2023-11-15 22:26:03 +0300

    mantıksal operatörler projesi eklendi

---
+++ C#-Programlama-Dili/04-MantiksalOperatorlerveIfElse/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _04_MantiksalOperatorlerveIfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kullanıcı adınızı giriniz :");
+            string kullaniciAdi = Console.ReadLine();
+            Console.WriteLine("Şifrenizi girin :");
+            string sifre = Console.ReadLine();
+
+
+            //if (kullaniciAdi == "aykut")
+            //{
+            //    Console.WriteLine("Kullanıcı adı doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Kullanıcı adı hatalı");
+            //}
+
+
+            //if (kullaniciAdi != "aykut")
+            //{
+            //    Console.WriteLine("Kullanıcı adı hatalı");
+            //}
+
+
+            //if (kullaniciAdi == "aykut")
+            //    Console.WriteLine("Kullanıcı adı doğru");
+            //else
+            //    Console.WriteLine("Kullanıcı adı hatalı");
+
+
+            //if (sifre == "123")
+            //    Console.WriteLine("Şifre doğru");
+            //else
+            //    Console.WriteLine("Şifre hatalı");
+
+
+
+            //if (kullaniciAdi == "aykut")
+            //{
+            //    Console.WriteLine("Kullanıcı adı doğru");
+
+            //    if (sifre == "123")
+            //        Console.WriteLine("Şifre doğru");
+            //    else
+            //        Console.WriteLine("Şifre hatalı");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Kullanıcı adı hatalı");
+            //}
+
+
+            //if (kullaniciAdi == "aykut" && sifre == "123")
+            //    Console.WriteLine("Giriş başarılı");
+            //else
+            //    Console.WriteLine("Kullanıcı adı veya şifresi hatalı");
+
+
+            if (kullaniciAdi != "aykut" || sifre != "123")
+                Console.WriteLine("Kullanıcı adı veya şifresi hatalı");
+            else
+                Console.WriteLine("Giriş yaptınız");
+
+
+
+
+
+
+            Console.WriteLine("Devam etmek istiyor musunuz ? (E/H)");
+            string degisken1 = Console.ReadLine();
+            if (degisken1 == "E" || degisken1 == "e")
+                Console.WriteLine("Evet");
+            else
+                Console.WriteLine("Hayır");
+
+        }
+    }
+}
