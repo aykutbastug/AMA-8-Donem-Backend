6f4ad2eae572506f0a7309a1681f6c58ce5e28f5
aykutbastug <aykutbastug@gmail.com>
2024-01-17 23:01:26 +0300

    mvc örnekleri eklendi

---
+++ ASP.NET MVC/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View("AboutDeneme");
+        }
+    }
+}
