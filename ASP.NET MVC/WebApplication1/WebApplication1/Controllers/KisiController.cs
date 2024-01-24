74d549ca84321d1e1290b4a95ffb0e05b257350f
aykutbastug <aykutbastug@gmail.com>
2024-01-24 22:50:01 +0300

    ADO.NET örnekleri eklendi

---
+++ ASP.NET MVC/WebApplication1/WebApplication1/Controllers/KisiController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class KisiController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        SqlConnection connection = new SqlConnection();
+
+        public KisiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connection.ConnectionString = _configuration.GetConnectionString("Rehber");
+        }
+
+        public IActionResult Index()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select * from dbo.Kisiler", connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            List<Kisi> Kisiler = new List<Kisi>();
+            foreach (DataRow row in dt.Rows)
+            {
+                Kisiler.Add(new Kisi
+                {
+                    Id = Convert.ToInt32(row["Id"].ToString()),
+                    ImageUrl = row["ImageUrl"].ToString(),
+                    Name = row["Name"].ToString(),
+                    Phone = row["Phone"].ToString(),
+                    Surname = row["Surname"].ToString()
+                });
+            }
+
+
+            return View(Kisiler);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Kisi model)
+        {
+            if (ModelState.IsValid)
+            {
+                SqlCommand cmd = new SqlCommand("insert into dbo.Kisiler values (@Name, @Surname, @Phone, @ImageUrl)", connection);
+                cmd.Parameters.AddWithValue("Name", model.Name);
+                cmd.Parameters.AddWithValue("Surname", model.Surname);
+                cmd.Parameters.AddWithValue("Phone", model.Phone);
+                cmd.Parameters.AddWithValue("ImageUrl", model.ImageUrl);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+                return View(model);
+        }
+    }
+}
---
+++ ASP.NET MVC/WebApplication1/WebApplication1/Models/Kisi.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplication1.Models
+{
+    public class Kisi
+    {
+        [DisplayName("Kişi No")]
+        [Required(ErrorMessage = "Id zorunlu alandır")]
+        public int Id { get; set; }
+
+
+        [DisplayName("Adı")]
+        [Required(ErrorMessage = "Adı zorunlu alandır")]
+        [MinLength(5, ErrorMessage = "Mininum 5 karakter girebilirsiniz.")]
+        [MaxLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string Name { get; set; }
+
+
+        [DisplayName("Soyadı")]
+        [Required(ErrorMessage = "Soyadı zorunlu alandır")]
+        [MaxLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string Surname { get; set; }
+
+        [DisplayName("Telefon")]
+        [Required(ErrorMessage = "Telefon zorunlu alandır")]
+        [MaxLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string Phone { get; set; }
+
+
+
+        [MaxLength(500, ErrorMessage = "Maksimum 500 karakter girebilirsiniz.")]
+        public string? ImageUrl { get; set; }
+    }
+}
